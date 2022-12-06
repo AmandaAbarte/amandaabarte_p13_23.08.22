9e88367cabf2602f51495902aa9e757d92bd38c8
AmandaAbarte <amanda.abarte@gmail.com>
2022-12-06 13:43:23 +0100

    axios

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+
+import reducers from "./reducers/index";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    reducers
   },
 });
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,68 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Login() {
+  const [token, setToken] = useState("");
+  const user = {
+    email: "steve@rogers.com",
+    password: "password456",
+  };
+  function getToken(e) {
+    e.preventDefault();
+    axios({
+      // Endpoint to send files
+      url: "http://localhost:3001/api/v1/user/login/",
+      method: "POST",
+
+      // Attaching the form data
+      data: user,
+    }) // Handle the response from backend here
+      .then((res) => {
+        console.log("res", res);
+        res.status === 200 && setToken(res.data.body.token);
+        getProfile(res.data.body.token);
+      })
+      .then((res) => {
+        console.log(token);
+      })
+      // Catch errors if any
+      .catch((err) => {
+        console.log("error", err);
+        console.log(err.toJSON());
+        err.toJSON().status === 400
+          ? console.log("Incorrect login data")
+          : console.log("server error, something went wrong");
+      });
+  }
+  function getProfile(testToken) {
+    console.log(testToken);
+    axios({
+      // Endpoint to send files
+      url: "http://localhost:3001/api/v1/user/profile/",
+      method: "POST",
+      headers: {
+        // Add any auth token here
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${testToken}`,
+      },
+      // // Attaching the form data
+    }) // Handle the response from backend here
+      .then((res) => {
+        console.log("res", res);
+        // res.status === 200 && setToken(res.data.body.token);
+      })
+
+      // Catch errors if any
+      .catch((err) => {
+        console.log("error", err);
+        // console.log(err.toJSON());
+        // err.toJSON().status === 400
+        //   ? console.log("Incorrect login data")
+        //   : console.log("server error, something went wrong");
+      });
+  }
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
@@ -19,10 +81,14 @@ export default function Login() {
             <input type="checkbox" id="remember-me" />
             <label>Remember me</label>
           </div>
-         {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
-          <a href="/profile" className="sign-in-button">Sign In</a>
+          {/* <!-- PLACEHOLDER DUE TO STATIC SITE --> */}
+          <a href="/profile" className="sign-in-button">
+            Sign In
+          </a>
           {/* !-- SHOULD BE THE BUTTON BELOW  */}
-          {/* <!-- <button className="sign-in-button">Sign In</button> --> */}
+          <button className="sign-in-button" onClick={getToken}>
+            Sign In
+          </button>
         </form>
       </section>
     </main>
