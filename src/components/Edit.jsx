5750868bf877fa8f9d133fb95007f274435c4c16
AmandaAbarte <amanda.abarte@gmail.com>
2022-12-24 12:26:32 -0300

    edit name and last name

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 export default function Edit() {
   const dispatch = useDispatch();
+
+  const token = useSelector((state) => state.token);
   const data = useSelector((state) => state.data);
+  const [formData, setFormData] = useState({
+    firstName: data.firstName,
+    lastName: data.lastName,
+  });
+  function updateName() {
+    axios({
+      url: "http://localhost:3001/api/v1/user/profile/",
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: formData,
+    }) // Handle the response from backend here
+      .then((res) => {
+        dispatch({
+          type: "getProfile",
+          data: res.data.body,
+        });
+      })
+      // Catch errors if any
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }
 
   return (
     <>
@@ -15,6 +44,9 @@ export default function Edit() {
           name="firstName"
           id="firstName"
           defaultValue={data.firstName}
+          onChange={(e) =>
+            setFormData({ ...formData, firstName: e.target.value })
+          }
         />
         <label htmlFor="lastName">Last Name</label>
         <input
@@ -22,7 +54,11 @@ export default function Edit() {
           name="lastName"
           id="lastName"
           defaultValue={data.lastName}
+          onChange={(e) =>
+            setFormData({ ...formData, lastName: e.target.value })
+          }
         />
+        <p>{formData.firstName}</p>
         <button
           className="edit-button"
           // on click change isEdit to false - hiding input fields
@@ -31,6 +67,7 @@ export default function Edit() {
               type: "isEdit",
               isEdit: false,
             });
+            updateName();
           }}
         >
           Save
